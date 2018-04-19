a75e808a8f649bb9dc1b65dd6e0cdc4d700aa7db
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-04-19 10:43:54 +0100

    Add first test

---
+++ test/calculator_test.php
@@ -0,0 +1,11 @@
+<?php
+
+class CalculatorTest extends PHPUnit_Framework_TestCase
+{
+  public function testAdd()
+  {
+    $c = New Calculator;
+    $result = $c->add(5, 10);
+    $this->assertEquals(15, $result);
+  }
+}
